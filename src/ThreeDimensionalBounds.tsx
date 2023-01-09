dc6f79808efd2fe8e394681e60a1032226eecfaa
Parker Stafford <parker@arize.com>
2023-01-09 11:22:35 -0800

    fix bounds calculation for orthographic camera

--- src/ThreeDimensionalBounds.tsx
+++ src/ThreeDimensionalBounds.tsx
@@ -1,6 +1,9 @@
 import React, { createContext, useContext, useEffect } from 'react';
 import { ThreeDimensionalBoundsType, ThreeDimensionalPoint } from './types';
-import { getCenterFromThreeDimensionalBounds } from './utils';
+import {
+  getCenterFromThreeDimensionalBounds,
+  isOrthographicCamera,
+} from './utils';
 import { useThree } from '@react-three/fiber';
 
 export type ThreeDimensionalBoundsContextType = {
@@ -24,6 +27,7 @@ export function ThreeDimensionalBounds({
   bounds,
   children,
   offset = 1.5,
+  boundsZoomPaddingFactor = 0.9,
 }: {
   children: React.ReactNode;
   /**
@@ -32,14 +36,23 @@ export function ThreeDimensionalBounds({
   bounds: ThreeDimensionalBoundsType;
   /**
    * Offset from the object to make it fit the bounds.
+   *
+   * Only affects perspective cameras.
    * @default 1.5
    */
   offset?: number;
-  }) {
+  /**
+   * The amount to reduce the zoom by to give some padding to the points (1 means no padding).
+   *
+   * Only affects orthographic cameras.
+   * @default 0.9
+   */
+  boundsZoomPaddingFactor?: number;
+}) {
   const center = getCenterFromThreeDimensionalBounds(bounds);
   const {
     camera,
-    // size: { width, height },
+    size: { width, height },
   } = useThree();
 
   useEffect(() => {
@@ -49,16 +62,26 @@ export function ThreeDimensionalBounds({
     const boundsHeight = maxY - minY;
     const boundsDepth = maxZ - minZ;
     const maxDim = Math.max(boundsWidth, boundsHeight, boundsDepth);
-    // @ts-ignore
-    const fov = camera.fov * (Math.PI / 180);
     camera.position.x = boundsWidth / 2 + minX;
     camera.position.y = boundsHeight / 2 + minY;
-    const cameraZ = maxDim / (2 * Math.tan(fov / 2));
-    camera.position.z = (cameraZ + center[2]) * offset;
+    if (isOrthographicCamera(camera)) {
+      const cameraZ = boundsDepth / 2 + minZ;
+      camera.position.z = cameraZ;
+      camera.zoom =
+        Math.min(width / boundsWidth, height / boundsHeight) *
+        boundsZoomPaddingFactor;
+      const cameraToNearEdge =
+        maxZ < 0 ? -maxZ + camera.position.z : camera.position.z - maxZ;
+      camera.near = cameraToNearEdge * 3;
+    } else {
+      // Perspective camera
+      const fov = camera.fov * (Math.PI / 180);
+      const cameraZ = maxDim / (2 * Math.tan(fov / 2));
+      camera.position.z = (cameraZ + center[2]) * offset;
+    }
 
     const cameraToFarEdge =
       minZ < 0 ? -minZ + camera.position.z : camera.position.z - minZ;
-
     camera.far = cameraToFarEdge * 3;
     camera.updateProjectionMatrix();
     camera.lookAt(...center);
