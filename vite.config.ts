bdda940277929cef13fdd7b411136802c6379c26
Tony Powell <apowell@arize.com>
2024-11-13 15:41:00 -0500

    refactor: Move off of tsdx and related tooling
    - Remove direct tsdx, babel, jest, tslib deps
    - Install vite, vitest, plugins
    - Configure vite and vitest
    - Update github workflows to use pnpm and node 20

---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import { resolve } from "path";
+import { defineConfig, mergeConfig } from "vite";
+import { defineConfig as defineVitestConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+const vitestConfig = defineVitestConfig({
+    test: {
+        globals: true,
+        include: ["test/**/*.{ts,tsx}"],
+    },
+});
+
+export default mergeConfig(
+    defineConfig({
+        plugins: [react()],
+        build: {
+            lib: {
+                // Could also be a dictionary or array of multiple entry points
+                entry: resolve(__dirname, "src/index.tsx"),
+                name: "PointCloud",
+                // the proper extensions will be added
+                fileName: "index",
+                formats: ["es", "cjs"],
+            },
+            minify: false,
+            rollupOptions: {
+                // make sure to externalize deps that shouldn't be bundled
+                // into your library
+                external: ["react", "react-dom"],
+                output: {
+                    // Provide global variables to use in the UMD build
+                    // for externalized deps
+                    globals: {
+                        react: "React",
+                        "react-dom": "ReactDOM",
+                    },
+                },
+            },
+        },
+    }),
+    vitestConfig,
+);
